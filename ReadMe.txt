237bbdaa340bb0bb5d10a04b931211e0428ec44a
Tarunteja Obbina <tarun@Taruntejas-MacBook-Pro.local>
2024-07-10 13:00:27 +0530

    final-commit

---
+++ ReadMe.txt
@@ -0,0 +1,24 @@
+ML Assignment 3
+---------------
+Team Members:
+
+Tarun Teja Obbina (txo220011)
+-----------------------------
+Submission Contents:
+
+Submission contains one python file (part2.py), one pdf file (part1.pdf) and a table with results (Results.pdf).
+------------------------------
+How to use the python file:
+
+The file contains "config" dictionary with all the parameters to update and run the k means clustering model.
+update the k array in the "config" dictionary as you wish and run the python file to see the SSE scores.
+The url for the dataset is https://drive.google.com/uc?id=1wAVFFqbd046uDKbSVPfvonA9iWKbq_es
+------------------------------
+libraries used:
+
+numpy==1.26.2
+pandas==2.1.3
+python-dateutil==2.8.2
+pytz==2023.3.post1
+six==1.16.0
+tzdata==2023.3
--- part2.py
+++ part2.py
@@ -1,50 +1,59 @@
 import re
 import random
+import pandas as pd
+# configurations
+config = {
+ 'k' : [5, 10, 15, 20, 25, 30, 35, 40],
+ 'dataset': 'https://drive.google.com/uc?id=1wAVFFqbd046uDKbSVPfvonA9iWKbq_es',
+ 'logging': False,
+ 'logFilePath': 'log.txt',
+ 'cleanData': True,
+ 'data': []
+}
 
+# function to pre process the tweets data file 
 def process_data_file(filePath: str):
  newFileLines = []
- regex = re.compile("http://bbc.in/*")
+ regex = re.compile("\@.+|http:\/\/*|\.\@.+") # regular expression to match http urls and words start with @
  
- try: 
-  file = open(filePath, 'r')
-  lines = file.readlines()
-  for line in lines:
-   words = [word.lower() for word in (line.split("|")[-1]).split(" ") if not regex.match(word)]
-   newFileLines.append(" ".join(words) + '\n')
-  file.close()
 
-  print("hi")
-  newFile = open('data.txt', 'w')
-  newFile.writelines(newFileLines)
-  newFile.close()
+ # file = open(filePath, 'r', encoding="cp1252")
+ file = pd.read_csv(filePath, delimiter="\t", header=None, encoding="cp1252")
+ # read all the lines in the dataset
+ for line in file[0].tolist():
+  words = [word.lower() for word in (line.split("|")[-1]).split(" ") if not regex.match(word)] # remove the words start with @ and http urls and time ids
+  newFileLines.append(" ".join(words) + '\n') # add them into the new file
 
- except:
-  print("unable to read the file.\n")
+ config['data'] = newFileLines
 
-# process_data_file('bbchealth.txt')
 
+# K Means Clustering Model
 class K_Means_Clustering:
- def __init__(self, k:int, dataset: str):
+ def __init__(self, k:int, logging= config['logging'], logFilePath= config['logFilePath']):
   self.k = k
-  self.dataset = dataset
   self.X = None
   self.data = None
   self.k_cluster = {}
+  self.logging = logging
+  self.logFilePath = logFilePath
 
+ # function to calculate the Jaccard Distance
  def distance(self, arr1: [], arr2: []):
   words1 = set(arr1)
   words2 = set(arr2)
+  # 1 - |A intersection B|/|A union B|
   return 1 - (len(words1.intersection(words2))/len(words1.union(words2)))
  
+ # function to load the data from the pre processed file
  def load_data(self):
   try:
-    datafile = open(self.dataset)
-    self.data = datafile.readlines()
+    self.data = config['data']
     self.X = [word[:-1].split(" ") for word in self.data]
     # print(self.X[0])
   except:
     print("error occurred while loading the data.")
   
+ # function to initialize the clusters dict for given value of k 
  def initialize_clusters(self):
   for i in range(self.k):
     self.k_cluster[i] = {
@@ -52,6 +61,7 @@ class K_Means_Clustering:
       'data': []
     }
 
+ # function to get the index of cluster for which given point is closer
  def getMinCenter(self, x: []):
   minI = self.k
   minDistance = 2
@@ -62,19 +72,24 @@ class K_Means_Clustering:
     minI = i
   return minI
  
+ # function to get the point that is closer to all the remaining points in a given cluster
  def getMinPoint(self, cluster: []):
   minI = len(cluster)
   minDistance = -1
+  # go through each point
   for point in range(len(cluster)):
    totalDistance = 0
    for point2 in range(len(cluster)):
+    # calculate the total distance of this point from all other points in the cluster
     if point != point2:
      totalDistance += self.distance(cluster[point], cluster[point2])
+   # find the point with smallest total Distance
    if minDistance == -1 or minDistance > totalDistance:
     minDistance = totalDistance
     minI = point
   return minI
  
+ # function to calculate the Sum of Squared Error
  def SSE(self):
   error = 0
   for i in range(self.k):
@@ -82,6 +97,7 @@ class K_Means_Clustering:
     error += (self.distance(point, self.k_cluster[i]['center']))**2
   return error
 
+ # function to initialize the clusters with random centroids
  def fit(self):
   #  step 1 - initializing
   self.initialize_clusters()
@@ -117,11 +133,19 @@ class K_Means_Clustering:
      self.k_cluster[i]['data'] = []
     prev_centroids = new_centroids 
    
-  # print([self.k_cluster[i]['center'] for i in range(self.k)])
-  print(self.SSE())
+  sse = self.SSE() 
+  print(self.k, " clusters with SSE = ", sse) 
+  if self.logging:
+   logFile = open(self.logFilePath, 'a')
+   logFile.write("k value: " + str(self.k) + "\n")
+   logFile.write("SSE Error: " + str(sse) + "\n")
+   logFile.writelines([(str(i) + "th cluster items: " + str(len(self.k_cluster[i]['data'])) + ", ") for i in range(self.k)] + ["\n"])
+   logFile.close()
+  
+if config['cleanData']:
+ process_data_file(config['dataset'])
 
-
-# print(test.distance("the long march", "ides of march"))
-model = K_Means_Clustering(250, 'data.txt')
-model.load_data()
-model.fit()
+for k in config['k']:
+ model = K_Means_Clustering(k)
+ model.load_data()
+ model.fit()
